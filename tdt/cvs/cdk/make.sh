57b1d5b1ebfc6adc6005fc40003c2df04afacb49
schpuntik <schpuntik@freenet.de>
2014-04-04 21:11:20 +0200

    small fix make.sh

--- tdt/cvs/cdk/make.sh
+++ tdt/cvs/cdk/make.sh
@@ -94,7 +94,7 @@ esac
 case "$REPLY" in
 	1) KERNEL="--enable-stm24 --enable-p0211";STMFB="stm24";;
 	2) KERNEL="--enable-stm24 --enable-p0213";STMFB="stm24";;
-	2) KERNEL="--enable-stm24 --enable-p0214";STMFB="stm24";;
+	3) KERNEL="--enable-stm24 --enable-p0214";STMFB="stm24";;
 	*) KERNEL="--enable-stm24 --enable-p0211";STMFB="stm24";;
 esac
 CONFIGPARAM="$CONFIGPARAM $KERNEL"
