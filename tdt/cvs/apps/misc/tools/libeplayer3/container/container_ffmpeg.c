f77760d4a1bd4b67c2b663f8dd3497d7af1c32a9
Taapat <taapat@gmail.com>
2014-04-10 20:56:28 +0300

    [libeplayer3] use resampling for aac
    aac without resampling may be used only if the sample rate is higher than 48000Hz, and rate in extradata is less than 5
    
    It's definitely spend more resources, but perhaps it is better than all the time to check it, and keep an additional code for this case.

--- tdt/cvs/apps/misc/tools/libeplayer3/container/container_ffmpeg.c
+++ tdt/cvs/apps/misc/tools/libeplayer3/container/container_ffmpeg.c
@@ -163,9 +163,9 @@ static char* Codec2Encoding(AVCodecContext *codec, int* version)
 			return "A_AC3";
 		case AV_CODEC_ID_DTS:
 			return "A_DTS";
+#if 0
 		case AV_CODEC_ID_AAC:
 			return "A_AAC";
-#if 0
 		case AV_CODEC_ID_WMAV1:
 		case AV_CODEC_ID_WMAV2:
 		case AV_CODEC_ID_WMAPRO:
@@ -1073,28 +1073,6 @@ int container_ffmpeg_update_tracks(Context_t *context, char *filename, int initi
 					}
 					else {
 						track.duration = (double) stream->duration * av_q2d(stream->time_base) * 1000.0;
-					}
-
-					if(stream->codec->codec_id == AV_CODEC_ID_AAC) {
-						unsigned int sample_index;
-
-						if(stream->codec->extradata_size >= 2) {
-							Hexdump(stream->codec->extradata, stream->codec->extradata_size);
-							sample_index = ((stream->codec->extradata[0] & 0x7) << 1)
-								+ (stream->codec->extradata[1] >> 7);
-						}
-						else {
-							sample_index = aac_get_sample_rate_index(stream->codec->sample_rate);
-						}
-
-						if ((sample_index > 4) || (stream->codec->extradata_size == 0)) { // I do not know if it is the right way, but works on all files which I tested
-							ffmpeg_printf(10,"aac sample index %d, use A_IPCM\n", sample_index);
-							encoding = "A_IPCM";
-							track.Encoding = "A_IPCM";
-						}
-						else {
-							ffmpeg_printf(10,"aac sample index %d, use A_AAC\n", sample_index);
-						}
 					}
 
 					if(!strncmp(encoding, "A_IPCM", 6))
