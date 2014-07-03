63ef390ea3a49faf7653e759543cd53d500c04c5
Taapat <taapat@gmail.com>
2014-07-03 21:09:55 +0300

    Update ntfs-3G to 2014.2.15

--- tdt/cvs/cdk/make/contrib-apps.mk
+++ tdt/cvs/cdk/make/contrib-apps.mk
@@ -240,7 +240,7 @@ $(DEPDIR)/usb_modeswitch_data: $(DEPDIR)/usb_modeswitch_data.do_compile
 #
 BEGIN[[
 ntfs_3g
-  2013.1.13
+  2014.2.15
   ntfs-3g_ntfsprogs-{PV}
   extract:http://tuxera.com/opensource/ntfs-3g_ntfsprogs-{PV}.tgz
   make:install:DESTDIR=PKDIR
